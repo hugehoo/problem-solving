12044678383177ea319bd0f4fa0bbe7ddfeacc85
tbnsok40 <tbnsok40@gmail.com>
2022-02-05 23:52:26 +0900

    set_list comprehension

--- 2022-02/05Feb 1043.py
+++ 2022-02/05Feb 1043.py
@@ -15,31 +15,28 @@ flag = True
 while flag:
     before = deepcopy(truth_total)
     for values in party_dict.values():
-        if truth_total & set(values):
-
-            # old
-            # for v in values:
-            #     truth_total.add(v)
-
-            # new
-            difference = set(values).difference(truth_total)
-            truth_total.update(difference)
+        if truth_total & set(values):  # intersection(truth_total, set(values))
+            truth_total = truth_total.union(set(values))
     after = truth_total
     if before == after:
         flag = False
 
-count_ = 0
-for k, v in party_dict.items():
-    if set(v) & truth_total:
-        continue
-    else:
-        count_ += 1
-print(count_)
+party_is_over = [1 for k, v in party_dict.items() if set(v).intersection(truth_total)]
+print(M - sum(party_is_over))
+
+
+# count_ = 0
+# for k, v in party_dict.items():
+#     if set(v) & truth_total:
+#         continue
+#     else:
+#         count_ += 1
+# print(count_)
 
 
 """
 4 5
-1 1 3
+1 1
 1 1
 1 2
 1 3

c836066bca85186946814a78b1edf8edfc9c1740
tbnsok40 <tbnsok40@gmail.com>
2022-02-12 13:52:57 +0900

    bagsack

--- 2022-02/11Feb 1010.py
+++ 2022-02/11Feb 1010.py
@@ -1,43 +1,12 @@
-import heapq
-from math import inf
 import sys
+from math import factorial
 
 input = sys.stdin.readline
-arr = [int(input()) for _ in range(9)]
-value = max(arr)
-print(value)
-print(arr.index(value) + 1)
+N = int(input())
 
-# N, M = map(int, input().split())
-# board = []
-# for _ in range(N):
-#     temp = []
-#     for i in list(map(int, input().split())):
-#         if len(temp) > 0:
-#             temp.append(i + temp[-1])
-#         else:
-#             temp.append(i)
-#     board.append(temp)
-# print(board)
+def solve(x, y):
+    return int(factorial(y) / (factorial(x) * factorial(y - x)))
 
-# board.append(list(map(int, input().split())))
-
-# for _ in range(M):
-#     x1, y1, x2, y2 = map(int, input().split())
-#     total = 0
-#     for y in range(x1 - 1, x2):
-#         for x in range(y1 - 1, y2):
-#             total += board[y][x]
-#
-#     print(total)
-
-"""
-4 3
-1 2 3 4
-2 3 4 5
-3 4 5 6
-4 5 6 7
-2 2 3 4
-3 4 3 4
-1 1 4 4
-"""
+for _ in range(N):
+    x, y = map(int, input().split())
+    (solve(x, y))

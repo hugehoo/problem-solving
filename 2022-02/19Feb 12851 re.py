06cbf2f35ed4bcd97169387902a35cbd631cc063
tbnsok40 <tbnsok40@gmail.com>
2022-02-19 21:41:16 +0900

    re

--- 2022-02/19Feb 12851 re.py
+++ 2022-02/19Feb 12851 re.py
@@ -5,24 +5,19 @@ input = sys.stdin.readline
 N, K = map(int, input().split())
 queue = deque()
 queue.append(N)
-# queue.append([N, 0, N])
-result = []
-visited = []
-dist = [0] * 100001
+dist = [-1] * 100001
+dist[N] = 0
 cnt = 0
 while queue:
     x = queue.popleft()
     if x == K:
         cnt += 1
-        # print(dist[x])
     for y in [x - 1, x + 1, 2 * x]:
-        if 0 <= y <= 100000 and (dist[y] == 0 or dist[y] >= dist[x] + 1):
-            # if dist[y] == 0 or dist[y] >= dist[x] + 1:
+        if 0 <= y <= 100000 and (dist[y] == -1 or dist[y] >= dist[x] + 1):
             dist[y] = dist[x] + 1
             queue.append(y)
 print(dist[K])
 print(cnt)
-
 """
 N - 1
 N + 1

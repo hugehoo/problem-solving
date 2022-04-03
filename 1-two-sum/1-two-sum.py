ac0964b1859bb20e081d7f3e1cbc33ab9ffd0052
Hoo <tbnsok40@gmail.com>
2022-04-03 20:21:42 +0900

    Time: 770 ms (35.04%), Space: 15 MB (77.48%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,9 +1,8 @@
 class Solution:
     def twoSum(self, nums, target: int):
         for i in range(len(nums)):
-            sub = target - nums[i]
             try:
-                find = nums.index(sub, i + 1)
+                find = nums.index(target - nums[i], i + 1)
             except ValueError:
                 continue
             return [i, find]

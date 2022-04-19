eb42d039252c159a582b5d93a6e66aacf13f2322
hugehoo <tbnsok40@gmail.com>
2022-04-20 07:48:41 +0900

    dp

---
+++ 2022-04/20APR 2579.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+stair, dp = [0] * 301, [0] * 301
+for i in range(n):
+    stair[i] = int(input())
+dp[0] = stair[0]
+dp[1] = stair[0] + stair[1]
+dp[2] = max(stair[1] + stair[2], stair[0] + stair[2])
+for i in range(3, n):
+    dp[i] = max(dp[i - 3] + stair[i - 1] + stair[i], dp[i - 2] + stair[i])
+print(dp[n - 1])
+"""
+# wrong
+cache = [1] * len(arr)
+for idx in range(len(arr)):
+    if arr[idx - 1] < arr[idx]:
+        cache[idx] = cache[idx - 1] + 1
+    else:
+        cache[idx] = cache[idx - 1]
+"""

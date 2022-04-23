61c419f3d6131698aad3834e02088678ce1773ab
hugehoo <tbnsok40@gmail.com>
2022-04-24 08:24:51 +0900

    bfs

--- 2022-04/22APR 10026.py
+++ 2022-04/22APR 10026.py
@@ -5,24 +5,19 @@ input = sys.stdin.readline
 
 N = int(input())
 board_ = [list(input().strip()) for _ in range(N)]
-mx = [1, -1, 0, 0]
-my = [0, 0, 1, -1]
-# visited = []
-visited_ = []
-
-
-# 같은것끼리만 넣으면 된다. 이미 방문한 노드는 넣지 말고..?
 
 
 def find(board):
     count = 0
-    visited = []
+    visited = [[0] * N for _ in range(N)]
     queue = deque()
     queue.append((0, 0))
-    visited.append((0, 0))
+    mx = [1, -1, 0, 0]
+    my = [0, 0, 1, -1]
+
     for r in range(N):
         for c in range(N):
-            if (r, c) not in visited:
+            if visited[r][c] == 0:
                 queue.append((r, c))
                 while queue:
                     r_, c_ = queue.popleft()
@@ -31,9 +26,9 @@ def find(board):
                         nx = c_ + mx[i]
                         if ny < 0 or ny >= N or nx < 0 or nx >= N:
                             continue
-                        if board[ny][nx] == board[r_][c_] and (ny, nx) not in visited:
+                        if board[ny][nx] == board[r_][c_] and visited[ny][nx] == 0:
                             queue.append((ny, nx))
-                            visited.append((ny, nx))
+                            visited[ny][nx] = -1
                 count += 1
     return count
 

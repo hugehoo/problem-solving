68df8a809c03baf8f744a323077c08dae3947f48
hugehoo <tbnsok40@gmail.com>
2022-04-26 09:14:12 +0900

    dp

--- 2022-04/25APR 등굣길.py
+++ 2022-04/25APR 등굣길.py
@@ -1,32 +1,25 @@
 from collections import deque
 
+"""
+m : 가로
+n : 세로
+"""
+
 
 def solution(m, n, puddles):
-    board = [[0 for _ in range(m)] for _ in range(n)]
-    for x, y in puddles:
-        board[x - 1][y - 1] = -1
-    queue = deque()
-    queue.append((0, 0))
+    puddles = [[q, p] for [p, q] in puddles]
+    dp = [[0] * (m + 1) for i in range(n + 1)]
+    dp[1][1] = 1
+    for r in range(1, n + 1):
+        for c in range(1, m + 1):
+            if r == 1 and c == 1:
+                continue
+            if [r, c] in puddles:
+                dp[r][c] = 0
+            else:
+                dp[r][c] = (dp[r - 1][c] + dp[r][c - 1]) % 1000000007
 
-    dx = [1, 0]
-    dy = [0, 1]
-    board[0][0] = 1
-    visited = []
-    for r in range(0, n):
-        for c in range(0, m):
-            # queue append 할 조건이 뭐지
-            while queue:
-                y, x = queue.popleft()
-                for i in range(2):
-                    nx = x + dx[i]
-                    ny = y + dy[i]
-                    if 0 <= nx < m and 0 <= ny < n and board[ny][nx] != -1: # and (ny, nx) not in visited:
-                        queue.append((ny, nx))
-                        # visited.append((ny, nx))
-                        board[ny][nx] += board[y][x]
-    for b in board:
-        print(b)
-    return
+    return dp[n][m]
 
 
 print(solution(4, 3, [[2, 2]]))

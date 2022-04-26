99995af4841272747f69c66561b9bac4264015ff
hugehoo <tbnsok40@gmail.com>
2022-04-26 10:37:29 +0900

    timeover

--- 2022-04/26APR 1655_timeover.py
+++ 2022-04/26APR 1655_timeover.py
@@ -1,11 +1,10 @@
 import sys
-from heapq import heappop, heappush
+from heapq import heappop, heappush, heapify
 
 input = sys.stdin.readline
 
 n = int(input())
 heap = []
-# print(heap[0])
 for _ in range(n):
     number = int(input())
     heappush(heap, (-number, number))
@@ -14,17 +13,15 @@ for _ in range(n):
     if rounds == 0:
         print(heap[0][1])
         continue
+
     for i in range(rounds):
         temp.append(heappop(heap))
-    # print(heappop(heap), temp, heap)
     pops = heappop(heap)
     print(pops[1])
     heappush(heap, pops)
-    # print(heappop(heap))
-    # heappop(heap) 프린트 한 것도 다시 넣어줘야지.
 
-    for t in temp:
-        heappush(heap, t)
+    heap += temp
+    heapify(heap)
 
 """
 7

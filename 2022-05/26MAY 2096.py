72c4d36e33f9b38606d69edf42cc784004b67958
hugehoo <tbnsok40@gmail.com>
2022-05-26 05:33:19 +0900

    dp

--- 2022-05/26MAY 2096.py
+++ 2022-05/26MAY 2096.py
@@ -4,35 +4,24 @@ from copy import deepcopy
 input = sys.stdin.readline
 
 M = int(input())
-board = []
-max_board = [[], []]
-min_board = [[], []]
-for m in range(M):
+_list = list(map(int, input().split()))
+
+max_board = [deepcopy(_list), [0, 0, 0]]
+min_board = [deepcopy(_list), [0, 0, 0]]
+
+for m in range(M - 1):
     _list = list(map(int, input().split()))
-    board.append(_list)
+    max_board[1][0] = max(max_board[0][0], max_board[0][1]) + _list[0]
+    max_board[1][1] = max(max_board[0][0], max_board[0][1], max_board[0][2]) + _list[1]
+    max_board[1][2] = max(max_board[0][1], max_board[0][2]) + _list[2]
+    max_board[1], max_board[0] = max_board[0], max_board[1]
 
-max_board[0] = deepcopy(board[0])
-min_board[0] = deepcopy(board[0])
-max_board[1] = [0, 0, 0]
-min_board[1] = [0, 0, 0]
+    min_board[1][0] = min(min_board[0][0], min_board[0][1]) + _list[0]
+    min_board[1][1] = min(min_board[0][0], min_board[0][1], min_board[0][2]) + _list[1]
+    min_board[1][2] = min(min_board[0][1], min_board[0][2]) + _list[2]
+    min_board[1], min_board[0] = min_board[0], min_board[1]
 
-for r in range(1, M):
-    for c in range(3):
-        if c == 0:
-            max_board[1][c] = max(max_board[0][c], max_board[0][c + 1]) + board[r][c]
-            min_board[1][c] = min(min_board[0][c], min_board[0][c + 1]) + board[r][c]
-        elif c == 1:
-            max_board[1][c] = max(max_board[0][c - 1], max_board[0][c], max_board[0][c + 1]) + board[r][c]
-            min_board[1][c] = min(min_board[0][c - 1], min_board[0][c], min_board[0][c + 1]) + board[r][c]
-        else:
-            max_board[1][c] = max(max_board[0][c], max_board[0][c - 1]) + board[r][c]
-            min_board[1][c] = min(min_board[0][c], min_board[0][c - 1]) + board[r][c]
-    if r == M - 1:
-        print(max(max_board[1]), min(min_board[1]))
-        break
-    max_board[0], max_board[1] = max_board[1], max_board[0]
-    min_board[0], min_board[1] = min_board[1], min_board[0]
-
+print(max(max_board[0]), min(min_board[0]))
 
 """
 위 아래 컬럼 두개를 만들어야 한다.

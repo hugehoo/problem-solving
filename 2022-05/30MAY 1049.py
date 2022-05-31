ddf6bf1e84c66811ef456442814b1cc4fbde3d64
hugehoo <tbnsok40@gmail.com>
2022-05-31 13:46:49 +0900

    greedy

--- 2022-05/30MAY 1049.py
+++ 2022-05/30MAY 1049.py
@@ -1,29 +1,23 @@
 import sys
+from math import inf
+
 
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-temp = [list(map(int, input().split())) for _ in range(M)]
-temp = sorted(temp, key=lambda x: (x[0], x[1],))
+min1 = inf
+min6 = inf
 
-"""
-사야할 갯수가 6개 이하일 때 분기 처리
-"""
-n, r = divmod(N, 6)
-answer = n * temp[0][0]
-if r * temp[0][1] > temp[0][0]:
-    val = temp[0][0]
-    temp = sorted(temp, key=lambda x: x[1])
+for _ in range(M):
+    six, one = map(int, input().split())
+    min1 = min(min1, one)
+    min6 = min(min6, six)
 
-    if val > temp[0][1] * r:
-        answer += (r * temp[0][1])
-        print(answer)
-        exit(0)
-    else:
-        answer += val
-        print(answer)
-        exit(0)
-else:
-    temp = sorted(temp, key=lambda x: x[1])
-    answer += (r * temp[0][1])
-    print(answer)
+b, r = divmod(N, 6)
+if min6 > min1 * 6:  # 1
+    print(N * min1)
+elif min6 < min1:  # 2
+    print(b * min6 + (min6 if r else 0))
+else: # 3
+    print(b * min6 + (min(r * min1, min6) if r else 0))
+

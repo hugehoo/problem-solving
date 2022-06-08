223a119243e0b48ab55b271879d90335c817c46e
hugehoo <tbnsok40@gmail.com>
2022-06-08 18:24:37 +0900

    graph

--- 2022-06/08JUN 7576.py
+++ 2022-06/08JUN 7576.py
@@ -1,7 +1,6 @@
 import sys
-from collections import defaultdict, deque
-from copy import deepcopy
-
+from collections import deque
+from itertools import chain
 
 input = sys.stdin.readline
 
@@ -9,7 +8,6 @@ C, R = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(R)]
 init_q = deque()
 visited = [[0] * C for _ in range(R)]
-count = 0
 
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
@@ -18,34 +16,17 @@ for r in range(R):
     for c in range(C):
         if board[r][c] == 1: init_q.append([r, c])
 
-while True:
-    another_q = deque()
-    while init_q:
-        y_, x_ = init_q.popleft()
-        visited[y_][x_] = 1
-        board[y_][x_] = "X"
-        for i in range(4):
-            ny = y_ + dy[i]
-            nx = x_ + dx[i]
-            if 0 > ny or R <= ny or nx < 0 or nx >= C:
-                continue
-            if board[ny][nx] == -1:
-                continue
-            if 0 <= ny < R and 0 <= nx < C and visited[ny][nx] == 0:
-                visited[ny][nx] = 1
-                board[ny][nx] = "X"
-                another_q.append([ny, nx])
-    count += 1
-    if not init_q and not another_q:
-        break
-    else:
-        init_q = deepcopy(another_q)
-
-for b in board:
-    if 0 in b:
+while init_q:
+    y_, x_ = init_q.popleft()
+    for i in range(4):
+        ny = y_ + dy[i]
+        nx = x_ + dx[i]
+        if 0 <= ny < R and 0 <= nx < C and board[ny][nx] == 0:
+            board[ny][nx] = board[y_][x_] + 1
+            init_q.append([ny, nx])
+for b in chain(*board):
+    if b == 0:
         print(-1)
         exit()
-print(count - 1)
-
-
-
+else:
+    print(max(chain(*board)) - 1)

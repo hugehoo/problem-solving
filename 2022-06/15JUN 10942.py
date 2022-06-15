2fd20e5d5c5cad7a48ffc03508bf87dd3f44c9b1
hugehoo <tbnsok40@gmail.com>
2022-06-15 19:50:02 +0900

    dp

--- 2022-06/15JUN 10942.py
+++ 2022-06/15JUN 10942.py
@@ -2,30 +2,23 @@ import sys
 
 input = sys.stdin.readline
 
-
-def palindrome(arr: list):
-    m = len(arr) // 2
-    if len(arr) % 2 == 0:
-        if arr[0:m] == arr[m: len(arr)][::-1]:
-            return 1
-        return 0
-    else:
-        if arr[0:m] == arr[m + 1: len(arr)][::-1]:
-            return 1
-        return 0
-
-
 if __name__ == "__main__":
     N = int(input())
     arr = list(map(int, input().split()))
     M = int(input())
+    dp = [[0] * N for _ in range(N)]
+
+    for num in range(N):
+        for start in range(N - num):
+            end = start + num
+
+            if start == end:
+                dp[start][end] = 1
+            elif arr[start] == arr[end]:
+                if (start + 1 == end) or (dp[start + 1][end - 1] == 1):
+                    dp[start][end] = 1
+
     for _ in range(M):
         a, b = map(int, input().split())
-        print(palindrome(arr[a - 1: b]))
+        print(dp[a - 1][b - 1])
 
-"""
-palindrome 인지 아닌지만 판별하면 된다. 어떻게?
-dp = []
-어떤 인덱스에서 몇번째까지 팰린드롬인지. 나오지 않을까? 한 인덱스를 기준으로 팰린드롬이 여러개 나올 수 있나?
- 
-"""

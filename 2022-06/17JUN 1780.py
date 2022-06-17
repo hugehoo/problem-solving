9b37623c3a0626f1b12e070f4f046e154093d4c6
hugehoo <tbnsok40@gmail.com>
2022-06-17 16:07:17 +0900

    end--no-edit

--- 2022-06/17JUN 1780.py
+++ 2022-06/17JUN 1780.py
@@ -30,14 +30,6 @@ def divide(board, N):
             #     divide(temp_ent, len(temp_ent[0]))
 
     print(answer)
-        # for row in board[mok * i: mok * (i + 1)]:
-        #     print("row ", row)
-        #     for j in range(3):
-        #         print("split row ", row[mok * j: mok * (j + 1)])
-        #         temp_ent.append(row[mok * j: mok * (j + 1)])
-        #     print(check(temp_ent))
-        # for j in range(3):
-        #     board[mok * i][mok * j]
 
     # [mok * 0  : mok * 1] [mok * 0  : mok * 1], [mok * 1  : mok * 2], [mok * 2  : mok * 3]
     # [mok * 1  : mok * 2]
@@ -51,8 +43,6 @@ if __name__ == "__main__":
     entire_board = [list(map(str, input().split())) for _ in range(t)]
     N = len(entire_board[0])
     divide(entire_board, N)
-    # for e in entire_board[0:3]:
-    #     print(e[0:3])
 
 """
 한변의 길이가 N (3**n) 일 때 3으로 나눴을 때의 몫이, 자르는 기준이 된다. 

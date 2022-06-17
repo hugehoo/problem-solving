c386054535924e6700cea5e1ce1a82be1e425598
hugehoo <tbnsok40@gmail.com>
2022-06-17 17:13:35 +0900

    timeover

--- 2022-06/17JUN 1780.py
+++ 2022-06/17JUN 1780.py
@@ -1,4 +1,5 @@
 import sys
+from collections import Counter
 from itertools import chain
 
 input = sys.stdin.readline
@@ -7,12 +8,14 @@ input = sys.stdin.readline
 def check(board):
     set_ = set()
     n = board[0][0]
+    if len(board[0]) == 1:
+        return board[0][0]
+
     for i in chain(*board):
         set_.add(i)
     if len(set_) == 1:
         return n
     else:
-
         return False
 
 
@@ -25,25 +28,23 @@ def divide(board, N):
             for row in board[mok * i: mok * (i + 1)]:
                 temp_ent.append(row[mok * j: mok * (j + 1)])
             if check(temp_ent) != False:
-                answer.append(check(temp_ent))
-            # else:
-            #     divide(temp_ent, len(temp_ent[0]))
+                counter.append(check(temp_ent))
+            else:
+                divide(temp_ent, len(temp_ent[0]))
 
-    print(answer)
-
-    # [mok * 0  : mok * 1] [mok * 0  : mok * 1], [mok * 1  : mok * 2], [mok * 2  : mok * 3]
-    # [mok * 1  : mok * 2]
-    # [mok * 2  : mok * 3]
-    return
+    return Counter(counter)
 
 
 if __name__ == "__main__":
-    answer = []
-    t = int(input())  # 3 9 27 81
+    counter = []
+    t = int(input())
     entire_board = [list(map(str, input().split())) for _ in range(t)]
     N = len(entire_board[0])
-    divide(entire_board, N)
+    answer = divide(entire_board, N)
+    print(answer['-1'])
+    print(answer['0'])
+    print(answer['1'])
 
 """
-한변의 길이가 N (3**n) 일 때 3으로 나눴을 때의 몫이, 자르는 기준이 된다. 
+한변의 길이가 N (3**n) 일 때 3으로 나눴을 때의 몫이, 자르는 기준이 된다.
 """

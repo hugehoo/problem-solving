b59c9e55c41a2f72acc01992b3cf35647901d01f
hugehoo <tbnsok40@gmail.com>
2022-07-12 23:18:01 +0900

    solved

--- 2022-07/07JUL 2559.py
+++ 2022-07/07JUL 2559.py
@@ -1,21 +1,22 @@
 import sys
 from collections import deque
-from copy import deepcopy
 
 input = sys.stdin.readline
-n, m = map(int, input().split())
+n, k = map(int, input().split())
 
 arr = list(map(int, input().split()))  # arr 의 길이 100_000
-max_v = sum(arr[0: 0 + m])
-if m == 1:
-    print(max(arr))
-    exit()
-elif n == m:
-    print(sum(arr))
-    exit()
+init_arr = deque()
+for i in range(k):
+    init_arr.append(arr[i])
+init_sum = sum(init_arr)
+result = init_sum
+for i in range(k, len(arr)):
+    pop = init_arr.popleft()
+    init_sum -= pop
+    new = arr[i]
+    init_sum += new
+    init_arr.append(new)
 
-for i in range(len(arr) - m + 1):
-    temp = arr[i: i + m]
-    result = sum(temp + temp[::-1]) // 2
-    max_v = max(max_v, result)  # slicing 은 결국 O(n)
-print(max_v)
+    result = max(result, init_sum)
+
+print(result)

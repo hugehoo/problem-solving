aa9be3b9e7a7e0bb79db512195ad61f6591bd572
hugehoo <tbnsok40@gmail.com>
2022-07-20 22:58:15 +0900

    fail

--- 2022-07/20JUL 9205.py
+++ 2022-07/20JUL 9205.py
@@ -1,21 +1,25 @@
+from collections import deque
+
+
 T = int(input())
 
 for _ in range(T):
     con = int(input())
-    con_list = []
+    con_list = deque()
 
     s_x, s_y = map(int, input().split())
     for _ in range(con):
         c_x, c_y = map(int, input().split())
+        con_list.append([c_x, c_y])
 
+    e_x, e_y = map(int, input().split())
+    while con_list:
+        c_x, c_y = con_list.popleft()
         distance = abs(c_x - s_x) + abs(c_y - s_y)
         if 1000 < distance:
             print("sad")
             exit()
-        con_list.append([c_x, c_y])
         s_x, s_y = c_x, c_y
-
-    e_x, e_y = map(int, input().split())
     distance = abs(e_x - s_x) + abs(e_y - s_y)
     if 1000 < distance:
         print("sad")
@@ -30,4 +34,21 @@ for _ in range(T):
 -450 -250
 -600 500
 -600 1000
+
+1
+2
+0 0
+-1000 0
+1000 0
+2000 0
+
+1
+0
+0 0
+2000 0
+
+1
+0
+0 0
+1000 0
 """

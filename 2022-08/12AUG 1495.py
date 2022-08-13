c13504319a9e0834bc1fbe47ae2fc30b23b9816c
hugehoo <tbnsok40@gmail.com>
2022-08-13 18:11:07 +0900

    dp

--- 2022-08/12AUG 1495.py
+++ 2022-08/12AUG 1495.py
@@ -5,7 +5,6 @@ N, S, M = map(int, input().split())
 V = list(map(int, input().split()))
 dp = [[0] * (M + 1) for _ in range(N + 1)]
 dp[0][S] = 1
-answer = -1
 
 for n in range(N):
     for m in range(M + 1):
@@ -14,10 +13,11 @@ for n in range(N):
                 dp[n + 1][m + V[n]] = 1
             if m - V[n] >= 0:
                 dp[n + 1][m - V[n]] = 1
-for idx, i in enumerate(dp[N]):
-    if i:
-        answer = idx
-print(answer)
+for i in range(len(dp[N]) - 1, -1, -1):
+    if dp[N][i]:
+        print(i)
+        exit()
+print(-1)
 
 """
 3 5 10

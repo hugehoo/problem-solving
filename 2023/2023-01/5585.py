f267dd3dd5c9cbf5a8d55793ab9d5fb7cc194a26
hugehoo <tbnsok40@gmail.com>
2023-01-05 00:17:07 +0900

    5585

--- 2023/2023-01/5585.py
+++ 2023/2023-01/5585.py
@@ -1,23 +1,24 @@
 N = int(input())
-arr = list(map(int, input().split()))
-max_num = -1e9
-dp = [[0] * N for _ in range(2)]
-dp[0][0] = arr[0]
-for i in range(1, N):
-    dp[0][i] = max(dp[0][i - 1] + arr[i], arr[i])
-    dp[1][i] = max(dp[0][i - 1], dp[1][i - 1] + arr[i])
-    max_num = max(max_num, dp[0][i], dp[1][i])
 
-print(max_num if N > 1 else arr[0])
+count = 0
+arr = [500, 100, 50, 10, 5, 1]
+N = 1000 - N
+for i in arr:
+    if N >= i:
+        count += (N // i)
+        N = N % i
+print(count)
 
 
-# 얘는 증가 부분 수열이었네. 지금 풀려는 문제는 연속 증가 수열이고.
-# dp = [0] * N
-# dp[0] = arr[0]
-# for i in range(1, N):
-#     dp[i] = arr[i]
-#     for j in range(i):
-#         if arr[j] < arr[i]:
-#             dp[i] = max(dp[i], dp[j] + arr[i])
-#             print(dp)
-# print(max(dp))
+"""
+1000 - 1 = 999
+999 - 500 = 499
+499 - 400 = 99
+99 - 90 = 9
+9 - 9 = 0
+
+500 : 500 * 1
+400 : 100 * 4
+90 : 50 * 1 + 10 * 4
+9 : 5 * 1 + 1 * 4
+"""

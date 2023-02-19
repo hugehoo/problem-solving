c617b0250111f14549c708d6bccc449b6d2b42fc
hoo <tbnsok40@gmail.com>
2023-02-19 14:47:10 +0900

    1182 backtracking ?

--- 2023/2023-02/1182.py
+++ 2023/2023-02/1182.py
@@ -1,25 +1,38 @@
 N, S = map(int, input().split())
 nums = list(map(int, input().split()))
 cnt = 0
+ans = []
 
 
-def backtracking(idx, partial_sum):
+def solve(start):
     global cnt
-    
-    if idx >= N:
-        return
-
-    partial_sum += nums[idx]
-
-    if partial_sum == S:
+    if sum(ans) == S and len(ans) > 0:
         cnt += 1
+    for i in range(start, N):
+        ans.append(nums[i])
+        solve(i + 1)
+        ans.pop()
 
-    backtracking(idx + 1, partial_sum)
-    backtracking(idx + 1, partial_sum - nums[idx])
 
-
-backtracking(0, 0)
+solve(0)
 print(cnt)
+# def backtracking(idx, partial_sum):
+#     global cnt
+#
+#     if idx >= N:
+#         return
+#
+#     partial_sum += nums[idx]
+#
+#     if partial_sum == S:
+#         cnt += 1
+#
+#     backtracking(idx + 1, partial_sum)
+#     backtracking(idx + 1, partial_sum - nums[idx])
+#
+#
+# backtracking(0, 0)
+# print(cnt)
 
 
 """

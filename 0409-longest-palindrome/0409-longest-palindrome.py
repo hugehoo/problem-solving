c4d96d82bc6c034f0f7b054e3ff2cc2bce346e2a
Hoo <tbnsok40@gmail.com>
2023-04-24 17:31:01 +0900

    Time: 39 ms (26.36%), Space: 13.8 MB (51.73%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -4,6 +4,4 @@ class Solution:
     def longestPalindrome(self, s: str) -> int:
         char_counts = Counter(s)
         result = sum(v // 2 * 2 for v in char_counts.values())
-        if result < len(s):
-            result += 1
-        return result
+        return result + 1 if result < len(s) else result

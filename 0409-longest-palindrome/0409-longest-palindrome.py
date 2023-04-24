75b797f5eef69822812acc58329918da37d0cc5e
Hoo <tbnsok40@gmail.com>
2023-04-24 17:27:37 +0900

    Time: 31 ms (81.03%), Space: 13.7 MB (94.64%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -1,19 +1,9 @@
+from collections import Counter
+
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        dic = {}
-        for s_ in list(s):
-            dic[s_] = dic.get(s_, 0) + 1
-        result = 0
-        odds = []
-        for v in dic.values():
-            if v % 2 == 0:
-                result += v
-            else:
-                odds.append(v)
-        odds.sort(reverse=True)
-        if odds and odds[0] == 1:
-            return result + 1
-        elif odds:
-            total = sum(num - 1 for num in odds[1:])
-            return result + odds[0] + total
+        char_counts = Counter(s)
+        result = sum(v // 2 * 2 for v in char_counts.values())
+        if result < len(s):
+            result += 1
         return result

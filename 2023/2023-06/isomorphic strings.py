e9f1bc85c0305de674a93c57521adb4c2191af6f
Hoo <tbnsok40@gmail.com>
2023-06-30 20:19:19 +0900

    Time: 55 ms (84.74%), Space: 21.1 MB (57.64%) - LeetHub

---
+++ 2023/2023-06/isomorphic strings.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        s_dict = {}
+        s_set = set()
+        for s_, t_ in zip(list(s), list(t)):
+            if not s_dict.get(s_) and t_ not in s_set:
+                s_set.add(t_)
+                s_dict[s_] = t_
+            else:
+                if s_dict.get(s_) != t_:
+                    return False
+        return True
+    
+
+s = Solution()
+# print(s.isIsomorphic("egg", "add"))
+# print(s.isIsomorphic("foo", "var"))
+# print(s.isIsomorphic("paper", "title"))
+print(s.isIsomorphic("badc", "baba"))
+

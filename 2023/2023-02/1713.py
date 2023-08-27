248601c2bbc94c08b8ba9157baac8a4733570546
hoo <tbnsok40@gmail.com>
2023-08-27 20:19:19 +0900

    14225

---
+++ 2023/2023-02/1713.py
@@ -0,0 +1,21 @@
+N = int(input())
+T = int(input())
+nums = list(map(int, input().split()))
+album = {}
+for i in range(T):
+    if nums[i] in album:
+        album[nums[i]][0] += 1
+    else:
+        if len(album) < N:
+            album[nums[i]] = [1, i]
+        else:
+            temp = sorted(album.items(), key=lambda x: (x[1][0], x[1][1]))
+            del album[temp[0][0]]
+            album[nums[i]] = [1, i]
+album = sorted(album.keys())
+print(*album)
+"""
+3
+9
+2 1 4 3 5 6 2 7 2
+"""

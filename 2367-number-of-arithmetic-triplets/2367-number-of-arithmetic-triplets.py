ed424a975279f3c9a00fa0f1af117641ade86790
Hoo <tbnsok40@gmail.com>
2023-11-12 20:52:26 +0900

    Time: 75 ms (35.09%), Space: 16.1 MB (93.93%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.py
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.py
@@ -3,10 +3,12 @@ class Solution:
         count = 0
         for i in range(0, len(nums) - 2):
             for j in range(i + 1, len(nums) - 1):
-                if nums[j] - nums[i] != diff:
+                if nums[j] - nums[i] == diff:
+                    for k in range(j + 1, len(nums)):
+                        if nums[k] - nums[j] == diff:
+                            count += 1
+                        else:
+                            continue
+                else:
                     continue
-                for k in range(j + 1, len(nums)):
-                    if nums[k] - nums[j] != diff:
-                        continue
-                    count += 1
         return count

de445a080ec56ac8127f36f4217db8fb48b88a6d
hoo <tbnsok40@gmail.com>
2023-11-15 01:42:44 +0900

    231115

--- 2023/2023-11/counting bits.py
+++ 2023/2023-11/counting bits.py
@@ -1,17 +1,27 @@
 class Solution:
     def countBits(self, n: int) -> list[int]:
-        dp = []
-        for i in range(n + 1):
-            b_ = bin(i).split("0b")[1]
-            dp.append(list(b_).count('1'))
-            
+        dp = [0]
+        for i in range(1, n + 1):
+            if i % 2 == 1:
+                dp.append(dp[i - 1] + 1)
+            else:
+                dp.append(dp[i // 2])
         return dp
 
 
 s = Solution()
 print(s.countBits(2))
 print(s.countBits(5))
+print(s.countBits(10))
+print(s.countBits(11))
 """
+0
+1
+1 2
+1 2 2 3
+1 2 2 3 2 3 3 4
+1 2 2 3 2 3 3 4 2 3 2 4 3 4 4 5
+
 0
 1
 10
@@ -21,4 +31,36 @@ print(s.countBits(5))
 110
 111
 1000
+1001
+1010
+1011
+1100
+1101
+1110
+1111
+10000
+10001
+10010
+10011
+10100
+10101
+10110
+10111
+11000
+11001
+11010
+11011
+11100
+11101
+11110
+11111
+
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+[0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2,  3,   2, 3,  3,  4,   1]
+0
+1
+1 2
+1 2 2 3
+1 2 2 3 2 3 3 4
+1 2 2 3 2 3 3 4 2 3 2 4 3 4 4 5
 """

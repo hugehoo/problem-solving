3704e7b61f60b3fc4a831a721e28b9f78956d31a
hoo <tbnsok40@gmail.com>
2023-12-20 02:19:15 +0900

    231220

--- 2023/2023-12/Is Subsequence.py
+++ 2023/2023-12/Is Subsequence.py
@@ -1,27 +1,13 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        if len(s) > len(t):
-            return False
-        set_s = set(s)
-        set_t = set(t)
-        difference = set_t.__contains__(set_s)
-        if not difference:
-            return False
-        sequence = 0
         for s_ in s:
-            if t.__contains__(s_):
-                if sequence <= t.index(s_):
-                    sequence = t.index(s_)
-                else:
-                    return False
-            else:
+            idx = t.find(s_)
+            if idx == -1:
                 return False
+            t = t[idx + 1:]
         return True
     
 
 s = Solution()
-print(s.isSubsequence("aaaaa", "bbbbb"))
-print(s.isSubsequence("abc", "ahbgdc"))
-print(s.isSubsequence("axc", "ahbgdac"))
-
-
+print(s.isSubsequence("abc", "ahabc"))
+print(s.isSubsequence("abdx", "ahabcd"))

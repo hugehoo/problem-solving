171acf82a6529a3d509b0737f8ceb3f4e811be9f
Hoo <tbnsok40@gmail.com>
2023-12-31 22:55:58 +0900

    Time: 0 ms (100.00%), Space: 2.6 MB (38.29%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.go
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.go
@@ -1,21 +1,22 @@
 func mergeAlternately(word1 string, word2 string) string {
-	res := ""
-	i, j := 0, 0
-	for i < len(word1) && j < len(word2) {
-		res += string(word1[i]) + string(word2[j])
-		i++
-		j++
-	}
+    res := ""
+    i, j := 0, 0
+    for i < len(word1) && j < len(word2) {
+        res += string(word1[i]) + string(word2[j])
+        i++
+        j++
+    }
 
-	for i < len(word1) {
-		res += string(word1[i])
-		i++
-	}
+    for i < len(word1){
+        res += string(word1[i])
+        i++
+    }
 
-	for j < len(word2) {
-		res += string(word2[j])
-		j++
-	}
-
-	return res
+    for j < len(word2){
+        res += string(word2[j])
+        j++
+    }
+    
+    return res
 }
+

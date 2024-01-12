0205893fb4e154c0bc44cd28bbb2a25beabcc04b
Hoo <tbnsok40@gmail.com>
2024-01-13 00:05:18 +0900

    Time: 35 ms (7.57%), Space: 7.1 MB (15.54%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.go
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.go
@@ -1,13 +1,13 @@
 func searchBST(root *TreeNode, val int) *TreeNode {
-    switch {
-        case root==nil:
-            return nil
-        case root.Val==val:
-            return root
-        case root.Val<val:
-            return searchBST(root.Right, val)
-        case root.Val>val:
-            return searchBST(root.Left, val)
-    }
+	switch {
+  case root == nil:
     return nil
+	case root.Val == val:
+		return root
+	case root.Val < val:
+		return searchBST(root.Right, val)
+	case root.Val > val:
+		return searchBST(root.Left, val)
+	}
+	return nil
 }

fc8146e0957de6318cccbaf2ec8bf3814b1824fa
Hoo <tbnsok40@gmail.com>
2024-02-02 23:16:41 +0900

    Time: 37 ms (59.05%), Space: 16.6 MB (54.33%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -2,6 +2,6 @@ class Solution:
     def hammingWeight(self, n: int) -> int:
         # Convert the integer to its binary representation as a string
         binary_representation = bin(n)[2:]
-
+        print(binary_representation)
         # Return the count of '1's in the binary representation
         return binary_representation.count('1')

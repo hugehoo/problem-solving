9332ff365d41e569bec39d3c57796ea3f7f87e0f
hoo <tbnsok40@gmail.com>
2024-02-02 23:20:48 +0900

    code 정리

--- 2024/[leetcode] Number of 1 Bits.py
+++ 2024/[leetcode] Number of 1 Bits.py
@@ -1,7 +1,3 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        # Convert the integer to its binary representation as a string
-        binary_representation = bin(n)[2:]
-        print(binary_representation)
-        # Return the count of '1's in the binary representation
-        return binary_representation.count('1')
+        return bin(n)[2:].count('1')

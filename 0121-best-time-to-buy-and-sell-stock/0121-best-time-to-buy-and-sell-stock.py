5c42a1ee53b4d2544bced83ee9fb8cc78a4845b9
Hoo <tbnsok40@gmail.com>
2024-05-01 13:17:53 +0900

    Time: 768 ms (41.77%), Space: 27.5 MB (34.04%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,12 +1,8 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        min_price = prices[0]
         max_profit = 0
-        for price in prices[1:]:
-            max_profit = max(max_profit, price - min_price)
+        min_price = prices[0]
+        for price in prices:
+            max_profit = max(price - min_price, max_profit)
             min_price = min(min_price, price)
         return max_profit
-        
-# 오름차순인 부분 수열을 찾아서 
-# 피봇 -> 나머지 절반에서 가장 큰 값을 찾고 차감
-# 나머지 절반을 어케 찾을거임?

07b4409836cd5c9044552ad27242f689a61dd270
Hoo <tbnsok40@gmail.com>
2024-05-02 23:12:28 +0900

    Time: 1614 ms (16.82%), Space: 21.9 MB (6.53%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -4,18 +4,17 @@ class Solution:
         print(nums)
         result = set()
         for s in range(len(nums) - 2):
-            start = nums[s]
-            m = s + 1
-            e = len(nums) - 1
+            m, e = s + 1, len(nums) - 1
             while m < e:
-                temp = start + nums[m] + nums[e]
+                temp = nums[s] + nums[m] + nums[e]
                 if temp > 0:
                     e -= 1
                 elif temp < 0:
                     m += 1
                 else:
-                    e_ = (start, nums[m], nums[e])
+                    e_ = ((nums[s]), nums[m], nums[e])
                     result.add(e_)
                     m += 1
                     e -= 1
         return [list(r) for r in result]
+    

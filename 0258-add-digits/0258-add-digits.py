4b4089b216ebd21ec8f93a5815c5788b5943a913
Hoo <tbnsok40@gmail.com>
2024-06-02 16:23:13 +0900

    Time: 24 ms (9.03%), Space: 11.7 MB (37.15%) - LeetHub

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -1,9 +1,9 @@
+from functools import reduce
+
+
 class Solution(object):
     def addDigits(self, num):
         string_num = str(num)
         while len(string_num) > 1:
-            result = 0
-            for s in string_num:
-                result += int(s)
-            string_num = str(result)
+            string_num = str(reduce(lambda x, y: int(x) + int(y), string_num))
         return int(string_num)

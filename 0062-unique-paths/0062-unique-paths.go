dc97208233a43ee4859da0e4f178ca86f57c5875
Hoo <tbnsok40@gmail.com>
2024-06-19 23:28:03 +0900

    Time: 0 ms (100.00%), Space: 2.4 MB (53.53%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.go
+++ 0062-unique-paths/0062-unique-paths.go
@@ -1,23 +1,21 @@
-
 func uniquePaths(m int, n int) int {
-	dp := make([][]int, n*m)
+	// Initialize a 2D slice (matrix) with dimensions m x n
+	dp := make([][]int, m)
 	for i := range dp {
 		dp[i] = make([]int, n)
 	}
 
+	// Set the number of paths for the first row and first column to 1
 	for i := 0; i < m; i++ {
 		for j := 0; j < n; j++ {
 			if i == 0 || j == 0 {
 				dp[i][j] = 1
+			} else {
+				dp[i][j] = dp[i-1][j] + dp[i][j-1]
 			}
 		}
 	}
 
-	for i := 1; i < m; i++ {
-		for j := 1; j < n; j++ {
-			dp[i][j] = dp[i-1][j] + dp[i][j-1]
-		}
-	}
-
+	// Return the number of unique paths to the bottom-right corner
 	return dp[m-1][n-1]
 }

09f8c64c243ac51bf63b2775a7812b6920d8969f
Hoo <tbnsok40@gmail.com>
2024-06-20 23:34:00 +0900

    Time: 31 ms (78.32%), Space: 16.4 MB (47.64%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.py
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.py
@@ -8,16 +8,15 @@
 class Solution:
     def myGuess(self, lower: int, upper: int) -> int:
         return (lower + upper) >> 1
+
     def guessNumber(self, n: int) -> int:
         lowerBound, upperBound = 1, n
-        # myGuess = (lowerBound+upperBound) >> 1
         myGuess = self.myGuess(lowerBound, upperBound)
         while (res := guess(myGuess)) != 0:
             if res == 1:
                 lowerBound = myGuess+1
             else:
                 upperBound = myGuess-1
-            # myGuess = (lowerBound+upperBound) >> 1
             myGuess = self.myGuess(lowerBound, upperBound)
     
         return myGuess
